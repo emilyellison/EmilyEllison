12e31c950b9f65394555a7dbd4bbada5d73516f0
Emily Ellison <emilyellison986@gmail.com>
2012-07-09 12:20:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmilyEllison::Application.config.secret_token = 'b383cbf032662e311f5f1ada4c9f7c2073bcb749348dc4387415789dcf6e8e405af50c7344cb911141678ac84353b3b631eb3e231b1c71fa46acbd514bcfcd9e'
