046d30f2163fe6264c10dfcdaa2523f801537c74
Emily Ellison <emilyellison986@gmail.com>
2012-07-23 23:30:18 -0500

    Show Which Tab is Active

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :find_current_location
+  
+  def find_current_location
+    @current_location = request.path_parameters
+    controller = @current_location[:controller]
+    action = @current_location[:action]
+    if controller == 'overview'
+      if action == 'index'
+        @home_page = 'active'
+      elsif action == 'about'
+        @about_page = 'active'
+      end
+    end
+  end
 end
