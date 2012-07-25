f31dfe1dab7b162ef8a5e98b55469e8b57fa7348
Emily Ellison <emilyellison986@gmail.com>
2012-07-24 19:54:35 -0500

    Polish Blog Posts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,7 @@
 class AdminController < ApplicationController
   
+  before_filter :ensure_administrator
+  
   def index
     
   end
--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,7 +1,7 @@
 class OverviewController < ApplicationController
   
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = BlogPost.order('created_at desc').limit(5).all
   end
   
   def about
