f4838fec9fb91f787cb38c9c5c21921471d4481b
Emily Ellison <emilyellison986@gmail.com>
2012-07-26 11:14:33 -0500

    Stylize Carousel

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,88 @@
-$(document).ready( function() {
-	$('.content_wrap').hide()
-	$('.content_wrap').fadeIn(500);
+$(document).ready(function() {
+
+
+		// Fade in on page load.
+	
+		$('.content_wrap').hide()
+		$('.content_wrap').fadeIn(500);
+
+		// Project carousel.
+
+    //rotation speed and timer
+    var speed = 5000;
+    var run = setInterval('rotate()', speed);   
+     
+    //grab the width and calculate left value
+    var item_width = $('#slides .list_items').outerWidth(); 
+    var left_value = item_width * (-1); 
+         
+    //move the last item before first item, just in case user click prev button
+    $('#slides .list_items:nth-child(1)').before($('#slides .list_items:last'));
+     
+    //set the default item to the correct position 
+    $('#slides .list').css({'left' : left_value});
+ 
+    //if user clicked on prev button
+    $('#prev').click(function() {
+ 
+        //get the right position            
+        var left_indent = parseInt($('#slides .list').css('left')) + item_width;
+ 
+        //slide the item            
+        $('#slides .list').animate({'left' : left_indent}, 300,function(){    
+ 
+            //move the last item and put it as first item               
+            $('#slides .list_items:nth-child(1)').before($('#slides .list_items:last'));           
+ 
+            //set the default item to correct position
+            $('#slides .list').css({'left' : left_value});
+         
+        });
+ 
+        //cancel the link behavior            
+        return false;
+             
+    });
+ 
+  
+    //if user clicked on next button
+    $('#next').click(function() {
+         
+        //get the right position
+        var left_indent = parseInt($('#slides .list').css('left')) - item_width;
+         
+        //slide the item
+        $('#slides .list').animate({'left' : left_indent}, 300, function () {
+             
+            //move the first item and put it as last item
+            $('#slides .list_items:last').after($('#slides .list_items:nth-child(1)'));                  
+             
+            //set the default item to correct position
+            $('#slides .list').css({'left' : left_value});
+         
+        });
+                  
+        //cancel the link behavior
+        return false;
+         
+    });        
+     
+    //if mouse hover, pause the auto rotation, otherwise rotate it
+    $('#slides').hover(
+         
+        function() {
+            clearInterval(run);
+        }, 
+        function() {
+            run = setInterval('rotate()', speed);   
+        }
+    ); 
+         
 });
+ 
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin :)  
+function rotate() {
+    $('#next').click();
+}
+
