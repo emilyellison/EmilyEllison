aa55834f5d87a7620adda459e280d3cb198a6a3e
Emily Ellison <emilyellison986@gmail.com>
2012-07-31 17:05:01 -0500

    Move All Edit Delete Functionality to Admin Page Only

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,8 @@ class AdminController < ApplicationController
   before_filter :ensure_administrator
   
   def index
-    
+    @blog_posts = BlogPost.order('created_at desc').all
+    @projects = Project.order('created_at desc').all
   end
   
 end
