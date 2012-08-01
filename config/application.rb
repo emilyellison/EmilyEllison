3b1bba4e11fe602d9f50ef3d946f7fe158f0f594
Emily Ellison <emilyellison986@gmail.com>
2012-07-31 22:17:49 -0500

    Fix Heroku Precompile

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module EmilyEllison
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Fix Heroku Precompile
+    config.assets.initialize_on_precompile = false
   end
 end
