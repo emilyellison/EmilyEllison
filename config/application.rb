891219c9e48d16fa7e58f6148192948dbd38bd50
Emily Ellison <emilyellison986@gmail.com>
2012-07-31 22:56:04 -0500

    Set Debug to False in Development

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,5 @@ module EmilyEllison
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    # Fix Heroku Precompile
-    config.assets.debug = false
   end
 end
