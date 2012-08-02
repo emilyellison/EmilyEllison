1a971eba917866bc51c05ed375a723bf35051c1c
Emily Ellison <emilyellison986@gmail.com>
2012-08-02 11:14:12 -0500

    Add jQuery Accordion to Portfolio Index

---
+++ app/assets/javascripts/special.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+		// Fade in on page load.
+	
+		$('.content_wrap').hide()
+		$('.content_wrap').fadeIn(500);
+      
+		// Display portfolio with accordion.
+		
+		$("#accordion").accordion();
+
+});
