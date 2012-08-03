f6934ae9e63a13c7fbe2c60be299726bc1a5d602
Emily Ellison <emilyellison986@gmail.com>
2012-08-02 21:24:57 -0500

    Get Rid of Remnant Image/Video in Views

--- app/models/project_image.rb
+++ app/models/project_image.rb
@@ -2,4 +2,7 @@ class ProjectImage < ActiveRecord::Base
   attr_accessible :image, :project_id
   
   belongs_to :project
+  
+  validates :image, presence: true
+  validates :project_id, presence: true
 end
