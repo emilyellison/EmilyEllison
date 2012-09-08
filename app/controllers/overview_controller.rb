78ffcdaf5cc4fb1d60ce1042c56e0697a0596f29
Emily Ellison <emilyellison986@gmail.com>
2012-09-08 09:53:52 -0500

    Take out Twitter for now

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -8,7 +8,7 @@ class OverviewController < ApplicationController
   end
   
   def about
-    @tweet = JSON.parse(open('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=emilyellison986&count=1').read)[0]
+
   end
   
 end
